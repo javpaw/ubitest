6b55c2b84cf3aa05e8bd436ca92072a620664d80
javier <thisisnotmy@ema.il>
2012-12-08 12:52:08 -0500

    acceso a primeras funciones del api

--- experiments/exp1.py
+++ experiments/exp1.py
@@ -1,4 +1,17 @@
 import ubiClient as ub
 from apiKey import api_key
-print api_key
 client = ub.ConnectionUbidots()
+client.authKey(api_key)
+
+#Obteniendo los sitios
+sites= client.getSites()['sites']
+
+
+#Obteniendo ids de  variables
+for site in sites:
+  vari = client.getVariables(site['domain'])['variables']
+  ids = [vr['id']  for vr in vari]
+  print ids
+
+#num_pages = client.getValues
+#obtener todos los datos de un sensor:
