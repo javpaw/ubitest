a3d9be455ce3c15bdf8eb389af3ffc1bb738070f
javier <thisisnotmy@ema.il>
2012-12-09 15:16:58 -0500

    se pulio mas la grafica que muestra los sensores

--- ubitest/sensors/static/sensors/js/directives.js
+++ ubitest/sensors/static/sensors/js/directives.js
@@ -9,11 +9,13 @@ angular.module('ubiApp.directives', []).
   }]).
   directive('jvVisualization',[function(){
     
-    var m = [80,80,80,80],
-    cwidth = 960,
+    var m = [10,10,100,40],
+    m2 = [430,10,20,40],
+    cwidth = 920,
     cheight = 500,
     width = cwidth-m[1]-m[3],
     height = cheight-m[0]-m[2],
+    height2 = cheight - m2[0] - m2[2],
     color = d3.interpolateRgb("#f77", "#77f");
 
     var numlines = 2;
@@ -23,74 +25,135 @@ angular.module('ubiApp.directives', []).
       restrict:'A',
       link:function(scope,elements,attrs){
         //Crear SVG Inicial
+        var values =[];
+        for(var i=0; i<numlines;i++){
+            var ln = []
+            for(var j=0; j<10;j++){
+              ln.push( {date: new Date(2012,0,1,j,0), value:Math.random()*10
+              }); 
+
+            }
+          values.push(ln)
+        }
+
+        //#Generador de lineas
+        var line = d3.svg.line()
+          .x(function(d) { return x(d.date); })
+          .y(function(d) { return y(d.value); });
+
+        var nav_line = d3.svg.line()
+          .x(function(d) { return x2(d.date); })
+          .y(function(d) { return y2(d.value); });
+
+
         
+        //#Eje X superior
+        console.log(width);
+        var x = d3.time.scale().range([0,width]);
+        var xAxis = d3.svg.axis().scale(x).tickSubdivide(true);
+        x.domain([values[0][0].date, values[0][values[0].length - 1].date]);
+        
+        //#Eje X inferior
+        var x2 = d3.time.scale().range([0,width]);
+        var xAxis2 = d3.svg.axis().scale(x2).tickSubdivide(true);
+        x2.domain([values[0][0].date, values[0][values[0].length - 1].date]);
+        
+        //#Eje Y superior
+        var y = d3.scale.linear().range([height, 0]),
+        yAxis = d3.svg.axis().scale(y).ticks(4).orient("left");
+        y.domain([0,10]);
+        //#Eje Y Inferior
+        var y2 = d3.scale.linear().range([height2, 0]),
+        yAxis2 = d3.svg.axis().scale(y2).ticks(4).orient("left");
+        y2.domain([0,10]);
+
+        //#Definicon del Evento Brush
+        var brush = d3.svg.brush()
+            .x(x2)
+            .on("brush", brush);
+
+
+        //#Append del SVG inicial
         var graphic = d3.select(elements[0])
           .append("svg")
             .attr("width", width+m[1]+m[3])
             .attr("height",height+m[0]+m[2])
-          .append("svg:g")
-            .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+   
 
-        var x = d3.time.scale().range([0,width]);
-        var xAxis = d3.svg.axis().scale(x).tickSubdivide(true);
+        //#Group para el panel principal
+        var principal = graphic.append("g")
+            .attr("transform", "translate(" + m[3] + "," + m[0] + ")")
+            .attr("class","principal")
+            .attr("width",width)
+            .attr("height",height)
 
-        var values =[];
-        for(var i=0; i<numlines;i++){
-            line = []
-            for(var j=0; j<10;j++){
-              line.push( {date: new Date(2012,0,1,j,0), value:Math.random()*10
-              }); 
+        //#Group para el panel de navegacion
+        var nav = graphic.append('g')
+            .attr("transform","translate("+ m2[3] +"," + m2[0] + ")");
 
-            }
-          values.push(line)
-        }
-        x.domain([values[0][0].date, values[0][values[0].length - 1].date]);
-
-          // Add the x-axis.
-        graphic.append("svg:g")
+        //#Eje x panel principal
+        principal.append("svg:g")
           .attr("class", "x axis")
           .attr("transform", "translate(0," + height + ")")
           .call(xAxis);
 
-         var y = d3.scale.linear().range([height, 0]),
-            yAxis = d3.svg.axis().scale(y).ticks(4).orient("left");
+        //#Eje x panel navegacion
+        nav.append("svg:g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height2 + ")")
+          .call(xAxis2);
 
-        y.domain([0,10]);
-
-        // Add the y-axis.
-        graphic.append("svg:g")
+        //#Eje y panel principal
+        principal.append("svg:g")
             .attr("class", "y axis")
-            .attr("transform", "translate(" + 0 + ",0)")
             .call(yAxis);
 
-        // A line generator, for the dark stroke.
-        var line = d3.svg.line()
-            .interpolate("monotone")
-            .x(function(d) { return x(d.date); })
-            .y(function(d) { return y(d.value); });
 
-        var sensor = graphic.selectAll('.sensor')
+        //#Agregar un Layer por cada grafica
+        var layers = principal.selectAll('.layers')
           .data(values)
           .enter()
           .append("g")
-          .attr("class","sensor")
-           
-        // Add the line path.
-        var pth = sensor.append("svg:path")
-            .attr("class", "line")
-            .attr("clip-path", "url(#clip)")
-            .attr("d", function(d){return line(d)});
+          .attr("class","layers")
 
-        sensor.selectAll("circle")
-          .data(function(d){return d})
+
+        //#Agregar la linea correspondiente a cada layer
+        layers.append("svg:path")
+          .attr("class", "line")
+          .attr("clip-path", "url(#clip)")
+          .attr("d", function(d){return line(d)});
+
+
+
+        //#Agregar un Layer por cada grafica a navegacion
+        var nav_layers = nav.selectAll('.layers')
+          .data(values)
           .enter()
-          .append("circle")
-          .attr("class","circ")
-          .attr("r",3.5)
-          .attr("cx",function(d){return x(d.date)})
-          .attr("cy",function(d){return y(d.value)})
-          .style("fill","red")
+          .append("g")
+          .attr("class","layers")
 
+
+        //#Agregar la linea correspondiente a cada layer
+        nav_layers.append("svg:path")
+          .attr("class", "line")
+          .attr("clip-path", "url(#clip)")
+          .attr("d", function(d){return nav_line(d)});
+
+        //
+        nav.append("g")
+            .attr("class", "x brush")
+            .call(brush)
+          .selectAll("rect")
+            .attr("y", -6)
+            .attr("height", height2 + 7);
+
+
+        //#Funcion de brush
+        function brush(){
+          x.domain(brush.empty() ? x2.domain() : brush.extent());
+          layers.selectAll("path").attr("d", function(d){return line(d)});
+          principal.select(".x.axis").call(xAxis);
+        }
 
       }
     }
