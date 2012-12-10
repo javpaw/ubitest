ae3a5d04c876fc350e21e377a08bb78adc542e4b
javier <thisisnotmy@ema.il>
2012-12-09 21:35:20 -0500

    se corrigieron algunos errores

--- ubitest/ubitest/urls.py
+++ ubitest/ubitest/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from sensors.views import actualizar_todos
+from sensors.views import actualizar_todos,actualizar
 
 admin.autodiscover()
 
@@ -9,6 +9,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^$', 'django.views.generic.simple.direct_to_template',{'template':'sensors/sensors_index.html'}),
-	url(r'actualizar_todos', actualizar_todos)
+	url(r'actualizar_todos', actualizar_todos),
 	url(r'actualizar', actualizar)
 )
